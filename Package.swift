3641f191cf639a2d560d5046de4d5ed480e3bb5e
Mattias Holm <lorrden@openorbit.org>
2026-09-01 21:53:05 +0200

    Remove temporary hack

--- Package.swift
+++ Package.swift
@@ -34,11 +34,7 @@ let package = Package(
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0"))
     ],
     targets: [
-        .target(
-            name: "UniversalTabs",
-            dependencies: [.product(name: "ZIPFoundation", package: "ZIPFoundation")],
-            resources: [.copy("Resources/SFZ")]
-        ),
+        .target(name: "UniversalTabs"),
         .target(name: "UTABComposerCore"),
         .target(name: "UTABInstruments", dependencies: ["UTABComposerCore"]),
         .target(name: "UTABComposerDSL", dependencies: ["UTABComposerCore", "UTABInstruments"]),
